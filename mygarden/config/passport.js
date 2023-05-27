6e5829270f875a91820c00953905d0653767e892
Camille Marker <camillemarker@gmail.com>
2023-05-27 15:54:43 -0400

    Defined routes for authentication

--- mygarden/config/passport.js
+++ mygarden/config/passport.js
@@ -26,3 +26,11 @@ passport.use(
     }
   )
 )
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user._id)
+})
+
+passport.deserializeUser(async function (userId, cb) {
+  cb(null, await User.findById(userId))
+})
