6c987d23435c9a2335d48c16f93654bac636219f
Camille Marker <camillemarker@gmail.com>
2023-05-27 16:07:46 -0400

    Added login/logout UI

--- mygarden/server.js
+++ mygarden/server.js
@@ -33,6 +33,10 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(function (req, res, next) {
+  res.locals.user = req.user
+  next()
+})
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
