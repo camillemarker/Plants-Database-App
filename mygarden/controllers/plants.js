88ec5bebdaf8aaee0cec0c9697c858c2a8f7f2a5
Camille Marker <camillemarker@gmail.com>
2023-05-28 16:10:23 -0400

    Defined the route and coded the controller for the create function

--- mygarden/controllers/plants.js
+++ mygarden/controllers/plants.js
@@ -1,9 +1,19 @@
 const Plant = require('../models/plant')
 
 module.exports = {
-  new: newPlant
+  new: newPlant,
+  create
 }
 
 function newPlant(req, res) {
   res.render('plants/new', { errorMsg: '' })
+}
+
+async function create(req, res) {
+  try {
+    const plant = await Plant.create(req.body)
+    res.redirect(`/plants/${plant._id}`)
+  } catch (err) {
+    res.render('plants/new', { errorMsg: err.message })
+  }
 }
