55b2ac9075abc06a4e7ced7f37a4386637ccfbe9
Camille Marker <camillemarker@gmail.com>
2023-05-28 14:04:58 -0400

    Defined plant schema and compiled into a model

---
+++ mygarden/models/plant.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.schema
+
+const plantSchema = new Schema(
+  {
+    name: String,
+    plantType: String,
+    datePlanted: Date,
+    sunshineRec: String,
+    waterRec: String
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('Plant', plantSchema)
