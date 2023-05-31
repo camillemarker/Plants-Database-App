1f8bcad82d6ff542451538ae1b6c3cee69b6b0bf
Camille Marker <camillemarker@gmail.com>
2023-05-30 21:02:33 -0400

    Required ensureLoggedIn in routes/plants.js

--- mygarden/routes/plants.js
+++ mygarden/routes/plants.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const plantsCtrl = require('../controllers/plants')
+const ensureLoggedIn = require('../config/ensureLoggedIn')
 
 router.get('/', plantsCtrl.index)
 
